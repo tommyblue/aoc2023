029d03a39a38af54109312964d7f09be66bd2a12
Tommaso Visconti <tommaso.visconti@gmail.com>
2023-12-05 00:04:39 +0100

    ex1a

---
+++ 01a.ex
@@ -0,0 +1,43 @@
+# leggo file
+# loop sulle righe
+# per ogni riga:
+#  - tengo il primo numero
+#  - tengo l'ultimo
+#  - li sommo e li aggiungo alla somma totale
+
+defmodule AOC.D01A do
+  @digits Range.new(0, 9) |> Enum.map(&to_string/1)
+
+  def run do
+    {:ok, content} = File.read("01a.input")
+
+    content
+    |> String.split("\n")
+    |> Enum.map(&get_first_and_last_digit/1)
+    |> Enum.map(&get_calibration_value/1)
+    |> Enum.reduce(0, fn calibration_value, acc ->
+      acc + calibration_value
+    end)
+    |> IO.puts()
+  end
+
+  defp get_first_and_last_digit(line) do
+    line
+    |> String.codepoints()
+    |> Enum.filter(fn codepoint -> codepoint in @digits end)
+    |> Enum.map(&String.to_integer/1)
+    |> case do
+      [] -> [0, 0]
+      [d] -> [d, d]
+      [first_digit | tail] ->
+        [
+          first_digit,
+          tail |> Enum.reverse() |> hd()
+        ]
+    end
+  end
+
+  defp get_calibration_value([first_digit, last_digit]), do: first_digit * 10 + last_digit
+end
+
+AOC.D01A.run()
